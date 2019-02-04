6b273f3e2ec341629f50519d07ca62d52507f748
medo67 <45091642+medo67@users.noreply.github.com>
2019-02-05 01:46:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,13 +3,12 @@ const client = new Discord.Client();
 var prefix = "o";
 client.on('ready', () => {
    console.log(`----------------`);
-      console.log(`Tiger Gamer `);
+      console.log(`Desert Bot- Script By : i1Suhaib`);
         console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : Jack ' `);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Tiger Gamer `)//حقوق دايموند كودز
-client.user.setStatus("online")
-});
+client.user.setGame(` Tiger Gamer `,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
 
 client.login(process.env.BOT_TOKEN);
