144ab4596e6d5f03724d7bed3a94b684ea3948e6
medo67 <45091642+medo67@users.noreply.github.com>
2019-02-05 01:49:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,5 +10,5 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 client.user.setGame(` Tiger Gamer `,"http://twitch.tv/S-F")
 client.user.setStatus("dnd")
-
+});
 client.login(process.env.BOT_TOKEN);
