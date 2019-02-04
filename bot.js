38f26bfe99ead6c19068cbb2711d6431ff0c882b
medo67 <45091642+medo67@users.noreply.github.com>
2019-02-05 01:39:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,13 +3,13 @@ const client = new Discord.Client();
 var prefix = "o";
 client.on('ready', () => {
    console.log(`----------------`);
-      console.log ( Bot Script  by Jack`);
+      console.log(`Tiger Gamer `);
         console.log(`----------------`);
-      console.log( `ON ${{client.guilds.size} Servers '     Script By : Jack ' `);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : Jack ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Tiger_Gamer`,"http://twitch.tv/Dream")
-client.user.setStatus("dnd")
+client.user.setGame(`Chicago | Music 8 `)//حقوق دايموند كودز
+client.user.setStatus("online")
 });
 
 client.login(process.env.BOT_TOKEN);
