6e8b65c16cf2ed0b734e8c4f0a8f8827bff50479
medo67 <45091642+medo67@users.noreply.github.com>
2019-02-05 02:30:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(` Tiger Gamer `,"http://twitch.tv/S-F")
+client.user.setGame(` احب سارة `,"http://twitch.tv/S-F")
 client.user.setStatus("dnd")
 });
 client.login(process.env.BOT_TOKEN);
